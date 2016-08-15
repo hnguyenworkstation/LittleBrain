e8d5ec9151988385f963c3ceea8c225425826f2b
Hung Nguyen <hnguyenworkstation@gmail.com>
2016-08-14 21:51:51 -0400

    added files

---
+++ LittleBrain/src/littlebrain/UserInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package littlebrain;
+
+/**
+ *
+ * @author quang
+ */
+public interface UserInterface {
+    public String LOGIN = "Login";
+    public String REGISTER = "Register";
+    public String LOGINTOOLTIP = "Click to Login!";
+    public String REGISTERTOOLTIP = "Register an account with us!";
+    
+}
